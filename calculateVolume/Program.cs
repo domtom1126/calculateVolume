f4cd3d0d140ae5e70a2f89f99cd76d1be175b44a
domtom1126 <domtom1126@gmail.com>
2019-07-15 08:11:22 -0700

    Add project files.

---
+++ calculateVolume/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace calculateVolume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            int radiusSquared = radius * radius;
+
+            Console.WriteLine("What is the height");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            double pi = 3.14;
+
+            double totalVolume = height * radiusSquared * pi;
+
+            Console.WriteLine("The volume is {0}", totalVolume);
+        }
+    }
+}
