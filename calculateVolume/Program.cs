f8a9d9c91601a4e25fceef17b6584271d4fef881
domtom1126 <domtom1126@gmail.com>
2019-07-16 02:26:38 -0700

    Added menu as well as menu functionality. Also started the menu for the Prism shapes.

--- calculateVolume/Program.cs
+++ calculateVolume/Program.cs
@@ -6,10 +6,73 @@ namespace calculateVolume
     {
         static void Main(string[] args)
         {
-            calculateVolume();
+            bool displayMenu = true;
+            while (displayMenu == true)
+            {
+                displayMenu = DisplayMenu();
+            }
+            
         }
 
-        public static void calculateVolume()
+        static public bool DisplayMenu()
+        {
+            Console.WriteLine("What shape do you want the volume for?");
+            Console.WriteLine();
+            Console.WriteLine("1. Cylinder");
+            Console.WriteLine("2. Cube");
+            Console.WriteLine("3. Prism");
+            Console.WriteLine("4. Pyramid");
+            Console.WriteLine("5. Exit");
+            string result = Console.ReadLine();
+
+            if (result == "1")
+            {
+                calculateVolumeOfCylinder();
+                return true;
+            }
+            else if (result == "2")
+            {
+                calculateVolumeOfCube();
+                return true;
+            }
+            else if (result == "3")
+            {
+                calculateVolumeOfPrism();
+                return true;
+            }
+            else if (result == "4")
+            {
+                calculateVolumeOfPyramid();
+                return true;
+            }
+            else if (result == "5")
+            {
+                Console.Clear();
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please use a valid number");
+                return false;
+            }
+
+        }
+
+        public static void Prism()
+        {
+            Console.WriteLine("What kind of prism?");
+            Console.WriteLine();
+            Console.WriteLine("1. Rectangular Prism");
+            Console.WriteLine("2. Cube");
+            Console.WriteLine("3. Triangular Prism");
+            Console.WriteLine("3. Cylinder");
+             void RectangularPrism()
+            {
+
+            }
+        }
+
+        public static void calculateVolumeOfCylinder()
         {
             Console.WriteLine("What is the radius");
             int radius = Convert.ToInt32(Console.ReadLine());
@@ -22,8 +85,23 @@ namespace calculateVolume
 
             double totalVolume = height * radiusSquared * pi;
 
-            Console.WriteLine("The volume is {0}", totalVolume);
+            Console.WriteLine("The total volume is {0}", totalVolume);
 
+        }
+
+        public static void calculateVolumeOfCube()
+        {
+            
+        }
+
+        public static void calculateVolumeOfPrism()
+        {
+            Console.WriteLine("volume of a prism");
+        }
+
+        public static void calculateVolumeOfPyramid()
+        {
+            Console.WriteLine("volume of a pyramid");
         }
     }
 }
