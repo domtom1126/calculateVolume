207bb456ef888566a860823dca6b1ae5c72eaa48
domtom1126 <domtom1126@gmail.com>
2019-07-16 03:22:02 -0700

    Added full functionality including all pyramids and prisms.

--- calculateVolume/Program.cs
+++ calculateVolume/Program.cs
@@ -71,6 +71,47 @@ namespace calculateVolume
 
                 Console.WriteLine("The total volume of the retangular prism is {0}", rectangularPrismVolume);
             }
+            else if (PrismResult == "2")
+            {
+                Console.WriteLine("What is the height");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the width");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the length");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int cubeVolume = height * length * width;
+
+                Console.WriteLine("The total volume of the cube is {0}", cubeVolume);
+            }
+            else if (PrismResult == "3")
+            {
+                Console.WriteLine("What is the base");
+                int prismBase = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height of the base");
+                int heightOfBase = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height of the prism");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                int triangularPrismVolume = (prismBase*heightOfBase/2) * height;
+
+                Console.WriteLine("The total volume of the triangular prism is {0}", triangularPrismVolume);
+            }
+            else if (PrismResult == "4")
+            {
+                Console.WriteLine("What is the radius");
+                int radius = Convert.ToInt32(Console.ReadLine());
+                int radiusSquared = radius * radius;
+
+                Console.WriteLine("What is the height");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                double pi = 3.14;
+
+                double totalVolume = (pi*radiusSquared)*height;
+
+                Console.WriteLine("The total volume is {0}", totalVolume);
+            }
         }
 
         public static void Pyramid()
@@ -81,38 +122,50 @@ namespace calculateVolume
             Console.WriteLine("1. Square or Rectangular Pyramid");
             Console.WriteLine("2. Triangular Pyramid");
             Console.WriteLine("3. Cone");
-        }
 
-        public static void calculateVolumeOfCylinder()
-        {
-            Console.WriteLine("What is the radius");
-            int radius = Convert.ToInt32(Console.ReadLine());
-            int radiusSquared = radius * radius;
+            string PyramidResult = Console.ReadLine();
 
-            Console.WriteLine("What is the height");
-            int height = Convert.ToInt32(Console.ReadLine());
+            if (PyramidResult == "1") 
+            {
+                Console.WriteLine("What is the height");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the width");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the length");
+                int length = Convert.ToInt32(Console.ReadLine());
 
-            double pi = 3.14;
+                int rectangularPyramidVolume = (height * length * width)/3;
 
-            double totalVolume = height * radiusSquared * pi;
+                Console.WriteLine("The total volume of the square or retangular pyramid is {0}", rectangularPyramidVolume);
+            }
+            else if (PyramidResult == "2")
+            {
+                Console.WriteLine("What is the base");
+                int pyramidBase = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height of the base");
+                int heightOfBase = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height of the pyramid");
+                int height = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("The total volume is {0}", totalVolume);
+                int triangularPyramidVolume = ((pyramidBase*heightOfBase/2)*height)/3;
 
-        }
+                Console.WriteLine("The total volume of the triangular prism is {0}", triangularPyramidVolume);
+            }
+            else if (PyramidResult == "3")
+            {
+                Console.WriteLine("What is the radius");
+                int radius = Convert.ToInt32(Console.ReadLine());
+                int radiusSquared = radius * radius;
 
-        public static void calculateVolumeOfCube()
-        {
-            
-        }
+                Console.WriteLine("What is the height");
+                int height = Convert.ToInt32(Console.ReadLine());
 
-        public static void calculateVolumeOfPrism()
-        {
-            Console.WriteLine("volume of a prism");
-        }
+                double pi = 3.14;
 
-        public static void calculateVolumeOfPyramid()
-        {
-            Console.WriteLine("volume of a pyramid");
+                double coneVolume = (pi * radiusSquared * height)/3;
+
+                Console.WriteLine("The total volume is {0}", coneVolume);
+            }
         }
     }
 }
